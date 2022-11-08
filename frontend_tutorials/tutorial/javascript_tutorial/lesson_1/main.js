102c397ab6fbefdc1eb1aebb7ba76d016065c108
GiangNB2 <giangnb2@rikkeisoft.com>
2022-11-08 16:02:09 +0700

    demo js

---
+++ frontend_tutorials/tutorial/javascript_tutorial/lesson_1/main.js
@@ -0,0 +1,16 @@
+// alert("Hello World")
+
+/**
+ * Build in function:
+ 1. Alear
+ 2. Console
+ 3. Confirm
+ 4. Prompt
+ 5. Set timeout
+ 6. Set interval
+ * */ 
+
+var fullName = 'Giang Nguyen';
+var age = 24;
+alert(age);
+console.log(fullName)
