1b111e167c49bfc1387815889062762cae4a6d8a
GiangNB2 <giangnb2@rikkeisoft.com>
2022-11-25 17:07:43 +0700

    slider in intro page

--- Web-Application-Projects/React-MyCV-Webapp/src/components/Slider/Slider.js
+++ Web-Application-Projects/React-MyCV-Webapp/src/components/Slider/Slider.js
@@ -1,18 +1,29 @@
 import PropTypes from "prop-types";
 import styles from "./Slider.module.scss";
 import classNames from "classnames/bind";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const bindStyle = classNames.bind(styles);
-
 function Slider({ images }) {
   const [index, setIndex] = useState(0);
 
   let sliderItem = images[index];
+  let isActive1;
+  let isActive2;
+  let isActive3;
+  let isActive4;
+
+  useEffect(() => {
+    setTimeout(() => 
+      setIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      ), 1500
+    );
+
+    return () => {};
+  }, [index]);
 
   const onPrevClicked = function () {
     if (index > 0 && index < images.length) {
@@ -36,6 +47,28 @@ function Slider({ images }) {
     }
   }
 
+  if (index === 0) {
+    isActive1 = true;
+    isActive2 = false;
+    isActive3 = false;
+    isActive4 = false;
+  } else if (index === 1) {
+    isActive1 = false;
+    isActive2 = true;
+    isActive3 = false;
+    isActive4 = false;
+  } else if (index === 2) {
+    isActive1 = false;
+    isActive2 = false;
+    isActive3 = true;
+    isActive4 = false;
+  } else if (index === 3) {
+    isActive1 = false;
+    isActive2 = false;
+    isActive3 = false;
+    isActive4 = true;
+  }
+
   return (
     <div className={bindStyle("container")}>
       <div className={bindStyle("wrapper")}>
@@ -52,10 +85,54 @@ function Slider({ images }) {
         <button onClick={onNextClicked}>Next</button>
       </div>
       <div className={bindStyle("slide-show-dot")}>
-        <FontAwesomeIcon className={bindStyle("icon")} icon={faCircle} />
-        <FontAwesomeIcon className={bindStyle("icon-active")} icon={faCircle} />
-        <FontAwesomeIcon className={bindStyle("icon")} icon={faCircle} />
-        <FontAwesomeIcon className={bindStyle("icon")} icon={faCircle} />
+        <div className={bindStyle("dot")}>
+          <FontAwesomeIcon
+            style={isActive1 || { display: "none" }}
+            className={bindStyle("dot-active")}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={isActive1 && { display: "none" }}
+            className={bindStyle("dot-inactive")}
+            icon={faCircle}
+          />
+        </div>
+        <div className={bindStyle("dot")}>
+          <FontAwesomeIcon
+            style={isActive2 || { display: "none" }}
+            className={bindStyle("dot-active")}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={isActive2 && { display: "none" }}
+            className={bindStyle("dot-inactive")}
+            icon={faCircle}
+          />
+        </div>
+        <div className={bindStyle("dot")}>
+          <FontAwesomeIcon
+            style={isActive3 || { display: "none" }}
+            className={bindStyle("dot-active")}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={isActive3 && { display: "none" }}
+            className={bindStyle("dot-inactive")}
+            icon={faCircle}
+          />
+        </div>
+        <div className={bindStyle("dot")}>
+          <FontAwesomeIcon
+            style={isActive4 || { display: "none" }}
+            className={bindStyle("dot-active")}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={isActive4 && { display: "none" }}
+            className={bindStyle("dot-inactive")}
+            icon={faCircle}
+          />
+        </div>
       </div>
     </div>
   );
